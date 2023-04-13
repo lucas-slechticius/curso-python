08ec212100b06bef6bf3ea3423e7a62212abbc87
lucas-slechticius <slechticiuslucas@gmail.com>
2023-04-13 18:58:39 -0300

    Estudos de string

---
+++ Strings.py
@@ -0,0 +1,25 @@
+x = "pica de mel"
+y = "mano changes"
+
+print(x,y)
+
+cliente = "Gabriel"
+qty = "1"
+livro = "logica de programacao"
+valorLivro = "79,90"
+vendedor = "Marcu"
+comissao = "30,00"
+
+print("Olá," + cliente + " sua compra de"+ qty + " Livro")
+print(livro+"por R$"+valorLivro+" foi efetuado com sucesso!")
+
+b = " Ola Mundo"
+#Método String
+
+print(b.strip()) #Ele ignora os espaços
+
+c = "OLA MUNDO"
+print(c.lower()) #Ele deixa tudo minusculo, assim como .downCase
+
+d = "ola mundo"
+print(d.upper()) #Deixa tudo Maiusculo, assim como o .upperCase
