010a599d135a67752b2a274a693a2d9e3dc7f8b2
lucas-slechticius <slechticiuslucas@gmail.com>
2023-04-13 19:58:10 -0300

    tipos de dados.py

---
+++ tipos_de_dados.py
@@ -0,0 +1,20 @@
+'''
+Variáveis e os tipos de dados
+
+tipos de dados:
+
+tipos de texto: str
+tipos númericos: int, float e o complex
+
+tipos de sequencia: list, tuple, range
+tipos de mapeamento: dict
+tipos set: set, frozenset
+
+tipo booleano: bool
+tipos binários: bytes, bytearray, memoryview
+
+print()
+type()
+#casting float, int, str
+
+'''
