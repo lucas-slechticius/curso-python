1ff1d58f9235d861a29e0fb9eae03cf730680979
lucas-slechticius <slechticiuslucas@gmail.com>
2023-04-13 19:01:04 -0300

    Type()

---
+++ type.py
@@ -0,0 +1,16 @@
+a = "Ola Mundo"
+b = 5
+c = 3.
+d = 1+2j
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+## Resposta, função type mostra o tipo de dado.
+##
+#<class 'str'>
+#<class 'int'>
+#<class 'float'>
+#<class 'complex'>
