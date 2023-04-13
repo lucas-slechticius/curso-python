dadebf60ad963880a39db7d144161fbd75573692
lucas-slechticius <slechticiuslucas@gmail.com>
2023-04-13 19:10:08 -0300

    Casting.py

---
+++ casting.py
@@ -0,0 +1,23 @@
+#ind
+#Casting converte tipo de variavel para outro tipo
+
+x = int(2)
+y = int(2.8)
+z = int('2')
+
+print( x, y, z)
+
+#float
+
+a = float(2.3)
+b = float(2)
+c = float("2.3")
+d = float("2")
+
+print(a,b,c,d)
+
+h = str('1')
+j = str(1.1)
+k = str(1)
+
+print(type(h),type(j),type(k))
