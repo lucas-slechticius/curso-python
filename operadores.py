6b3a336abff15784bb847a4cdeb32aec55b7b6b2
lucas-slechticius <slechticiuslucas@gmail.com>
2023-04-13 21:05:01 -0300

    operadores.py

---
+++ operadores.py
@@ -0,0 +1,12 @@
+'''
+Operador de soma/adição +
+Operador de subtração -
+Operador de multiplicação *
+Operador de divisão /
+
+Operador do resto %
+Operador da exponenciação **
+Operador da divisão inteira //
+
+
+'''
