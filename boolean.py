99dca934b47bbf8dfcaef57213710264cb213b5e
lucas-slechticius <slechticiuslucas@gmail.com>
2023-04-13 22:00:14 -0300

    Boolean

---
+++ boolean.py
@@ -0,0 +1,22 @@
+'''
+Operadores lógicos e o tipo Boolean
+
+Em ciência da computação, boolean, ou lógico,
+é um tipo de dado primitivo que possui dois valores,
+que podem ser considerados como (0 ou 1), falso ou verdadeiro.
+
+Chamado boolean em homenagem a George Boole
+
+
+'''
+
+# Tipo Boolean : True e false
+
+'''
+Operadores Lógicos
+
+and (e)
+or (ou)
+not (negação)
+
+'''
