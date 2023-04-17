94f76ca33dfc843812302e275eb07000eecad34e
lucas-slechticius <slechticiuslucas@gmail.com>
2023-04-17 20:00:49 -0300

    Atualizando condicionais, com encurtamento de if, if else, e alinhamento de if

--- condicionais.py
+++ condicionais.py
@@ -24,12 +24,24 @@ Ou seja, não precisa necessariamente terminar com else
 apenas quando os 10 estiverem false que vai
 ser levado em conta o else)
 """
-
-
-
-
-
-
-
-
 print("codigo fora da condicional")
+
+a = 5
+b = 8
+c = 2
+
+if b > a:
+    print("b eh maior que a")
+
+    #como encurtar o if
+
+# Como encurtar com if e else
+print("B") if b < a else print("A")
+
+# Alinhamento de IF
+
+if a > b: #Usar um alinhamento de if garante nesse exemplo que A e maior que b e c
+    if a > c:
+        print("A")
+
+
