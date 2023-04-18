7125cc8fe295424c1a08f4f293ecef4283b2f8d8
lucas-slechticius <slechticiuslucas@gmail.com>
2023-04-18 10:20:09 -0300

    Fazendo um programa que reconhece se o numero é par ou impar

---
+++ par_impar.py
@@ -0,0 +1,12 @@
+'''
+Como descobrir se um número é impar ou par
+'''
+print(30 * "-")
+numero = int(input("Insira um numero para saber se o mesmo eh par: "))
+if (numero % 2) == 0:
+    print(f"{numero} eh um numero par")
+else:
+    print(f"{numero} eh um numero impar")
+print(30 * "-")
+
+
