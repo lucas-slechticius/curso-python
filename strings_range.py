1bfb97a808b7a235d839b6cfd973d4810053ede5
lucas-slechticius <slechticiuslucas@gmail.com>
2023-04-18 17:04:08 -0300

    Strings_range.py

---
+++ strings_range.py
@@ -0,0 +1,29 @@
+
+
+# ->    0123456
+nome = "chicago"
+
+print(nome[4])#<== chamei apenas o caractere "4"
+
+"""
+Fazer um range decrescente
+"""
+
+for x in range(10,0,-1):
+    print(x)
+
+"""
+TRAZENDO COMANDOS DA LINGUAGEM C PARA QUEBRA DE LINHA
+city = "Alvorada \n" 
+
+"""
+'''
+city = "Alvorada"
+city2 = "Mogi"
+
+print(city, end=" ")
+print(city2)
+'''
+
+for x in nome:
+    print(x, end=" ")
