f182813a8155aa3418f1a28845ac2fd1fd999331
lucas-slechticius <slechticiuslucas@gmail.com>
2023-04-18 09:10:09 -0300

    fazendo um verificador de numeros, se é positivo ou negativo

---
+++ verificandosinalnumerico.py
@@ -0,0 +1,8 @@
+numero = int(input("Digite um numero: "))
+
+if numero > 0:
+    print(f"O {numero} eh positivo")
+elif numero == 0:
+    print(f"O {numero} eh igual a zero")
+else:
+    print(f"O {numero} eh negativo")
