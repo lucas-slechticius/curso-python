c787c229c51cde1092e700776f616196d5955d2a
lucas-slechticius <slechticiuslucas@gmail.com>
2023-04-18 16:33:58 -0300

    Atribuições

---
+++ atribuicao.py
@@ -0,0 +1,23 @@
+'''
+Atribuição
+
+Sites para treinar Lógica e competir online:
+
+urionlinejudge.com.br
+hackerrank.com
+codechef.com
+codingame.com
+
+'''
+
+x = 1
+print("Valor de X anteriorente:",x)
+x += 3
+x -= 3
+x *= 3
+x **= 3
+x /= 3
+x %= 3
+x //= 3
+
+print("Valor de x apos a reescrita",x)
