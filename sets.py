087d4c73f92cf0d794ed0114376ff9958d430821
lucas-slechticius <slechticiuslucas@gmail.com>
2023-04-26 20:55:39 -0300

    Estudando SETS pt1

---
+++ sets.py
@@ -0,0 +1,24 @@
+'''
+Listas = coleção ordenada, mutável e que permite valores duplicados
+tuplas = coleção ordenada, imutável e que permite valores duplicados
+dicionarios = coleção não ordenada, mutável e que não permite valores duplicados
+sets = não ordenada, imutável e que não permite valores duplicados (BASICAMENTE NÃO PODE NADA)
+'''
+
+#Os sets também são conhecidos como conjuntos
+
+"""conjunto = {"item1", "item2", "item3"}
+print(type(conjunto))
+#<class 'set'>
+
+print(conjunto)
+print(len(conjunto))
+"""
+
+tupla = (1,2,3,14)
+
+conjunt = set(tupla)
+print(conjunt)
+
+conjunt = set ((2, 3, 5 , 9))
+print(conjunt)
