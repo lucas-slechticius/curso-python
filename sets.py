b3a31cb6c359d2496d1590d3602b74410e34f7a3
lucas-slechticius <slechticiuslucas@gmail.com>
2023-05-03 19:33:33 -0300

    Sets.py parte 2

--- sets.py
+++ sets.py
@@ -22,3 +22,65 @@ print(conjunt)
 
 conjunt = set ((2, 3, 5 , 9))
 print(conjunt)
+"""
+
+Como adicionar elementos
+
+
+set1 = {1, 2, 3, 4, 5}
+set2 = {"True", "False"}
+set1.update(set2)
+print(set1)
+set1.update({"teste", 1})
+print(set1)
+
+
+"""
+
+#Removendo Elementos
+
+set1 = {1, 2, 3, 4, 5, 6, 7, True, False, "True", "False", "Lucas"}
+
+#usando .pop e .remove
+"""
+set1.remove("Lucas")
+set1.discard(1)
+set1.pop()
+print(set1)
+"""
+
+#Funções principais
+
+set1 = {1,2,3,4,5,6,7,8,9}
+set2 = {9, 8, 7, "Nagatani", "True", "Teste"}
+
+"""
+set3 = set1.union(set2)
+
+print(set3)
+
+set1.update(set2)
+
+
+print(set1)"""
+
+#Isso resulta apenas em números repitidos.
+set3 = set1.intersection(set2)
+
+print(set3)
+
+
+#.intersection_update()Permite que você atualize o primeiro set.
+set1.intersection_update(set2)
+
+print(set1)
+
+
+#Mostra os itens que não estão repetidos
+set3 = set1.symmetric_difference(set2)
+
+print(set3)
+
+#Atualizar o proprio array
+set1.symmetric_difference_update(set2)
+print(set1)
